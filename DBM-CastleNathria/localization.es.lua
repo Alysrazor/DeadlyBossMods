9bc8bd81a946a29fa023e199c56074d47c8cb4f7
Alysrazor <sergiocabades131999@gmail.com>
2020-11-29 19:55:52 +0100

    Update localization.es.lua

--- DBM-CastleNathria/localization.es.lua
+++ DBM-CastleNathria/localization.es.lua
@@ -27,7 +27,7 @@ L:SetOptionLocalization({
 L= DBM:GetModLocalization(2418)
 
 L:SetMiscLocalization({
-	Phase2			= "The anticipation to use this relic is killing me! Though, it will more likely kill you.",
+	Phase2			= "¡Las ganas de usar la reliquia me están matando! Aunque imagino que os matará a vosotros.",
 	Phase2Demonic	= "Lok zennshinagas xi ril zila refir il rethule no Rakkas az alar alar archim maev shi ",--Boss has Curse of Tongues
 	Phase3			= "I hope this wondrous item is as lethal as it looks!",
 	Phase3Demonic	= "X ante zila romathis alar il re thorje re az modas "--Boss has Curse of Tongues
